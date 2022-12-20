268f9d2757c7ad48e121aa5018f395f22e4b3cca
Gabriel Ávila Rangel <gabriel@sevenapps.com.br>
2022-12-20 11:25:51 -0500

    feat: integrate user initial validation

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const onClickLogin = () => {
+const validateUser = async (email) => {
+  try {
+    const result = await fetch(
+      `https://mp-wallet-app-api.herokuapp.com/users?email=${email}`
+    );
+    const user = await result.json();
+    return user;
+  } catch (error) {
+    return { error };
+  }
+};
+
+const onClickLogin = async () => {
   const email = document.getElementById("input-email").value;
   if (email.length < 5 || !email.includes("@")) {
     alert("Email inválido!");
     return;
   }
-  localStorage.setItem("@WalletApp:userEmail", email);
+  const result = await validateUser(email);
+
+  if (result.error) {
+    alert("Falha ao validar e-mail.");
+    return;
+  }
+  localStorage.setItem("@WalletApp:userEmail", result.email);
+  localStorage.setItem("@WalletApp:userName", result.name);
+  localStorage.setItem("@WalletApp:userId", result.id);
   window.open("./src/pages/home/index.html", "_self");
 };
