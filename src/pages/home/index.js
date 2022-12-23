a620777ea960c5079b5ebe1bd54efc5a607b8ee7
Gabriel Ávila Rangel <gabriel@sevenapps.com.br>
2022-12-23 11:13:32 -0500

    feat: implement header at home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,29 @@
-window.onload = () => {
+const onLoadUserInfo = () => {
   const email = localStorage.getItem("@WalletApp:userEmail");
-  console.log(email);
+  const name = localStorage.getItem("@WalletApp:userName");
+
+  const navbarUserInfo = document.getElementById("navbar-user-container");
+  const navbarUserAvatar = document.getElementById("navbar-user-avatar");
+
+  // add user email
+  const emailElement = document.createElement("p");
+  const emailText = document.createTextNode(email);
+  emailElement.appendChild(emailText);
+  navbarUserInfo.appendChild(emailElement);
+
+  // add logout link
+  const logoutElement = document.createElement("a");
+  const logoutText = document.createTextNode("sair");
+  logoutElement.appendChild(logoutText);
+  navbarUserInfo.appendChild(logoutElement);
+
+  // add user first letter inside avatar
+  const nameElement = document.createElement("h3");
+  const nameText = document.createTextNode(name.charAt(0));
+  nameElement.appendChild(nameText);
+  navbarUserAvatar.appendChild(nameElement);
+};
+
+window.onload = () => {
+  onLoadUserInfo();
 };
